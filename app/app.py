8cd8ce87d066fd8f9bd1c7337d9a9ac1f2b213bd
oni <weathereport.wr@gmail.com>
2026-07-25 17:36:22 +0000

    feat: connect flask to postgresql

--- app/app.py
+++ app/app.py
@@ -1,9 +1,21 @@
 from flask import Flask, jsonify
 import os
+import psycopg2
+
 
 app = Flask(__name__)
 
 VERSION = os.getenv("APP_VERSION", "1.0.0")
+
+
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.getenv("DATABASE_HOST", "database"),
+        database=os.getenv("DATABASE_NAME", "platform"),
+        user=os.getenv("DATABASE_USER", "devops"),
+        password=os.getenv("DATABASE_PASSWORD", "devops")
+    )
+
 
 @app.route("/")
 def home():
@@ -20,12 +32,37 @@ def health():
     })
 
 
-@app.route("/version")
-def version():
-    return jsonify({
-        "version": VERSION
-    })
+@app.route("/database")
+def database():
+
+    try:
+        connection = get_db_connection()
+
+        cursor = connection.cursor()
+
+        cursor.execute(
+            "SELECT version();"
+        )
+
+        result = cursor.fetchone()
+
+        cursor.close()
+        connection.close()
+
+        return jsonify({
+            "database": "connected",
+            "version": result[0]
+        })
+
+    except Exception as error:
+        return jsonify({
+            "database": "error",
+            "message": str(error)
+        }), 500
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(
+        host="0.0.0.0",
+        port=8080
+    )
